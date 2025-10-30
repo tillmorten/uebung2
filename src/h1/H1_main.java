e9cc6d81e61a00efcd20e93eecc58aabe91f4cd0
unknown <trettig1@smail.uni-koeln.de>
2025-10-30 13:24:38 +0100

    first commit h

---
+++ src/h1/H1_main.java
@@ -0,0 +1,14 @@
+package h1;
+
+public class H1_main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int z =25;
+		if(z>10) {
+			z = z - 10;
+			
+		}System.out.println( + z);
+	}
+
+}
---
+++ src/p3/P3_main.java
@@ -0,0 +1,20 @@
+package p3;
+
+public class P3_main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			int j = 1, i = 2, k = 51;
+			
+			if(i<j) {
+				System.out.println("A");
+			}
+			if(i<k) {
+				System.out.println("B");
+			}
+			if(j<k) {
+				System.out.println("C");
+			}
+	}
+
+}
