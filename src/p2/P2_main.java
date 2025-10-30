b1d2103260bf1a53ca12c585f1e983299281bd41
unknown <trettig1@smail.uni-koeln.de>
2025-10-30 12:42:31 +0100

    first commit 2

---
+++ src/p2/P2_main.java
@@ -0,0 +1,18 @@
+package p2;
+
+public class P2_main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			int i = 9;
+			boolean b = false;
+			
+			if(i > 10){
+				b = true;
+				
+			
+			}
+			System.out.println(b);
+	}
+
+}
